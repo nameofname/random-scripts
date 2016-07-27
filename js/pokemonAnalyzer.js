51e1841015d3c28faab5ef7dc14e85deec7b2b80
nameofname <nameofemail@gmail.com>
2016-07-27 01:34:53 -0400

    added chainability - new filtering to pokemon analyzer.

--- js/pokemonAnalyzer.js
+++ js/pokemonAnalyzer.js
@@ -1,35 +1,75 @@
 "use strict"; 
 
+
 const data = require("./pokemonData.json");
 const log = require('./colorLog');
 
-data.forEach((o, idx) => { o.rank = idx; });
 
-const findByName = name => {
-    return data.reduce((prev, o) => {
+/**
+ * dataContainer is a factory for the exported object. Used here to promote chaining.
+ * examples :
+ *      const analyzer = require('./pokemonAnalyzer.js')
+ *      analyzer.getBottom(5)
+ *          .getTop(1)
+ *          .val();
+ *
+ * @param a
+ */
+const dataContainer = function (a) { this.value = a };
+dataContainer.prototype = Array;
+
+
+// assign a rank type array to each pokemon :
+data.forEach((o, idx) => { o.rank = idx; });
+data.forEach(o => {
+    o.type = o.type
+        .split(/(?=[A-Z])/)
+        .map(s => s.toLowerCase() )
+});
+
+
+dataContainer.prototype.val = function () { return this.value; };
+// dataContainer.prototype.log = function () { return log.green(JSON.stringify(this.value)); };
+
+
+dataContainer.prototype.findByName = function (name) {
+    const arr = data.reduce((prev, o) => {
         if (prev) {
             return prev
         } else {
             return o.name.toLowerCase() === name.toLowerCase() ? o : null 
         }
     }, null);
+    return new dataContainer(arr);
 };
 
-const getTop = n => {
+
+dataContainer.prototype.getTop = function (n) {
     const arr = [];
-    for (let i=0; i<=n; i++) {
-        arr.push(data[i]);
+    for (let i=0; i<n; i++) {
+        arr.push(this.value[i]);
     }
-    return arr;
+    return new dataContainer(arr);
 };
 
-const analyzer = {
-    findByName : findByName,
-    getTop : getTop
+
+dataContainer.prototype.getBottom = function (n) {
+    const arr = [];
+    const len = this.value.length;
+    for (let i=len; i>=len-n; i--) {
+        arr.unshift(this.value[i]);
+    }
+    return new dataContainer(arr);
 };
 
-module.exports = analyzer;
+dataContainer.prototype.filterByType = function (type) {
+    const arr = this.value.filter((o, idx) => {
+        return o.type.indexOf(type) !== -1;
+    });
+    return new dataContainer(arr);
+};
 
-// examples : 
-log.green(JSON.stringify(analyzer.getTop(30)));
-log.green(JSON.stringify(analyzer.findByName('Vileplume')));
+
+
+module.exports = new dataContainer(data);
+
