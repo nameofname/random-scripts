c282ee3b6d4e40e41f7228e36eb86e59cefec259
nameofname <nameofemail@gmail.com>
2017-02-11 15:56:45 -0500

    adding to solution for 2 pairs

--- js/gears/index.js
+++ js/gears/index.js
@@ -1,24 +1,48 @@
 "use strict";
 
+const calculateQuotients = require('./calculateQuotients');
 
 /**
  * The base formula we are solving for looks like this :
  *
- *      (a/b) * (c/d) ... * requiredFactor = desiredProduct
+ *      (a/b) * (c/d) ... * requiredFactor = desiredNumber
  *
  * Where increasing the numberOfPairs will result in another fraction being added ot the equation.
  *
  * Example :
  *
- *      solve(127, [40], 3);
+ *      solve({ desiredNumber: 127, requiredFactors : [40], numberOfPairs: 3, lowerBound: 13, upperBound: 50 });
  *      (a/b) * (c/d) * (e/f) * 40 = 127 // solves for the equation :
  *
- * The solution will be an array representing the values of a - f which results in the closest number to 127
+ * The solution will be an array representing the values a - f between 13 - 50
+ * which results in the closest number to 127
  *
- * @param desiredProduct
+ * @param desiredNumber
  * @param requiredFactors
  * @param numberOfPairs
+ * @param upperBound
+ * @param lowerBound
  */
-module.exports = (desiredProduct, requiredFactors, numberOfPairs) => {
-    //
+module.exports = ({ desiredNumber, requiredFactors, numberOfPairs, upperBound, lowerBound }) => {
+    const quotients = calculateQuotients({ upperBound, lowerBound });
+    let requiredFactor;
+    requiredFactor = requiredFactors.reduce((prev, val) => (prev * val), 1);
+    const desiredProduct = desiredNumber / requiredFactor;
+
+
+
+    // TEST!!!! First, let's start off by finding the solution for 2 pairs of gears :
+    quotients.forEach(obj => {
+        const q = obj.quotient;
+        const desiredBuddy = desiredProduct / q;
+        let distanceFromBuddy;
+
+        findBuddy:
+        for (var i = 0; i <= quotients.length; i++) {
+
+
+            break findBuddy;
+        }
+    });
+
 };
