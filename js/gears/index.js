9b579a2211f4df6c7380641ce57e0b1025272e34
nameofname <nameofemail@gmail.com>
2017-02-11 16:29:24 -0500

    one attempt at hashing out logic for solution with 2 gear pairs

--- js/gears/index.js
+++ js/gears/index.js
@@ -24,7 +24,7 @@ const calculateQuotients = require('./calculateQuotients');
  * @param lowerBound
  */
 module.exports = ({ desiredNumber, requiredFactors, numberOfPairs, upperBound, lowerBound }) => {
-    const quotients = calculateQuotients({ upperBound, lowerBound });
+    const sortedQuotients = calculateQuotients({ upperBound, lowerBound });
     let requiredFactor;
     requiredFactor = requiredFactors.reduce((prev, val) => (prev * val), 1);
     const desiredProduct = desiredNumber / requiredFactor;
@@ -32,17 +32,46 @@ module.exports = ({ desiredNumber, requiredFactors, numberOfPairs, upperBound, l
 
 
     // TEST!!!! First, let's start off by finding the solution for 2 pairs of gears :
-    quotients.forEach(obj => {
-        const q = obj.quotient;
-        const desiredBuddy = desiredProduct / q;
-        let distanceFromBuddy;
+
+    let product;
+    let solution;
+
+    sortedQuotients.forEach(obj => {
+        const desiredBuddy = desiredProduct / obj.quotient;
+        const findDistance = (quotient) => Math.abs(desiredBuddy - quotient);
+        let buddy = sortedQuotients[0];
+        let distanceFromBuddy = findDistance(buddy.quotient);
 
         findBuddy:
-        for (var i = 0; i <= quotients.length; i++) {
+        for (var i = 1; i <= sortedQuotients.length; i++) {
 
+            const curr = sortedQuotients[i].quotient;
+            const distance = findDistance(curr);
+            const gotCloser = distance < distanceFromBuddy;
 
-            break findBuddy;
+            if (gotCloser) {
+                distanceFromBuddy = distance;
+                buddy = sortedQuotients[i];
+            } else {
+                break findBuddy;
+            }
+        }
+
+        const currProduct = obj.quotient * buddy.quotient;
+
+        if (!product) {
+            product = currProduct;
+            solution = [obj, buddy];
+            return;
+        }
+
+        if (Math.abs(desiredProduct - currProduct) < Math.abs(desiredProduct - product)) {
+            product = currProduct;
+            solution = [obj, buddy];
+            return;
         }
     });
 
+
+    return solution;
 };
--- js/gears/test.js
+++ js/gears/test.js
@@ -1,7 +1,12 @@
 "use strict";
 
 
-let calculateQuotients = require('./calculateQuotients');
-const arr = calculateQuotients({ lowerBound : 3, upperBound : 10 });
+// let calculateQuotients = require('./calculateQuotients');
+// const arr = calculateQuotients({ lowerBound : 3, upperBound : 10 });
 // const arr = calculateQuotients({ lowerBound : 13, upperBound : 50 });
-console.log(arr);
+
+
+let solve = require('./index')
+let solution = solve({ desiredNumber: 127, requiredFactors : [40], numberOfPairs: 2, lowerBound: 13, upperBound: 50 });
+
+console.log(solution);
