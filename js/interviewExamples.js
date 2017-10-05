5dd3250939429e15f973c7c712a0e3bb12cfac8a
nameofname <nameofemail@gmail.com>
2017-10-05 00:37:57 -0400

    pallindrome function

---
+++ js/interviewExamples.js
@@ -0,0 +1,64 @@
+// global scope
+// var a is in function scope, b is global.
+(function() {
+    var a = b = 5;
+})();
+
+console.log(b);
+
+
+// defining native methods
+String.prototype.repeatify = function(int) {
+    var i = int;
+    var out = '';
+    while (i > 0) {
+        i--;
+        out += this;
+    }
+    return out;
+};
+
+console.log('hello'.repeatify(3));
+
+
+// hoisting
+// var a gets hoisted, but not defined, at runtime. Therefore, it's undefined instead of 'not defined' error
+function test() {
+    console.log(a);
+    console.log(foo());
+
+    var a = 1;
+    function foo() {
+        return 2;
+    }
+}
+
+test();
+
+
+// 'this' keyword :
+// this depends on the method of invocation
+// in a browser, var fullname is a property of window
+// however in Node.js - variables don't get assigned to the global object.
+var fullname = 'John Doe';
+var obj = {
+    fullname: 'Colin Ihrig',
+    prop: {
+        fullname: 'Aurelio De Rosa',
+        getFullname: function() {
+            return this.fullname;
+        }
+    }
+};
+
+console.log('getFullname 1', obj.prop.getFullname());
+
+var getName = obj.prop.getFullname;
+
+console.log('getFullname 2', getName()); // 'John Doe' in browser, undefined in Node.js
+
+console.log('getFullname 3', getName.apply(obj.prop)); // 'John Doe' in browser, undefined in Node.js
+
+
+
+
---
+++ js/pallindrome.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+const easyAnswer = str => {
+    return str === str.split('').reverse().join('');
+};
+
+
+const pallindrome = str => {
+    const arr = str.split('');
+    const middle = (arr.length / 2) + (arr.length % 1);
+    for (let i = 0; i < arr.length; i++) {
+        if (i !== middle && arr[i] !== arr[arr.length - (i + 1)]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+
+console.log(pallindrome('ronaldlanor'));
+console.log(pallindrome('manface'));
+console.log(pallindrome('abcba'));
+
+console.log(easyAnswer('ronaldlanor'));
+console.log(easyAnswer('manface'));
+console.log(easyAnswer('abcba'));
+
+
+module.exports = pallindrome;
