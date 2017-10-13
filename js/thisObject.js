c1c9d4acc634f6615d29392a78d645fcc9001d22
nameofname <nameofemail@gmail.com>
2017-10-13 14:29:22 -0400

    more examples

--- js/thisObject.js
+++ js/thisObject.js
@@ -3,6 +3,7 @@
 // this code doesn't work in node because there is no window, and global is not used by default as window is.
 
 this.test = 'the windows value';
+console.log(this.test === window.test);
 
 const functionExample = function (string) {
     console.log('==========================================');
@@ -31,12 +32,31 @@ const obj = {
     }
 };
 
-functionExample('ronald1'); // this = window
-obj.functionExample('ronald2'); // this = obj
-obj.subObj.functionExample('ronald3'); // this = obj.subObj
-obj.subObj.subObj.functionExample('ronald3'); // this = obj.subObj.subObj
+functionExample('ronald1');
+obj.functionExample('ronald2');
+obj.subObj.functionExample('ronald3');
+obj.subObj.subObj.functionExample('ronald3');
 
 console.log(window.test);
 console.log(obj.test);
 console.log(obj.subObj.test);
 console.log(obj.subObj.subObj.test);
+
+console.log('==========================================');
+console.log('==========================================');
+console.log('==========================================');
+
+this.test = 'the windows value';
+
+const arrowExample = () => {
+    console.log(this.test);
+};
+
+const obj1 = {
+    test: 'obj1 value',
+    arrowExample
+};
+
+// 'this' will be the window in both of the following cases :
+arrowExample();
+obj1.arrowExample();
