a9721868469fcd3f11dd9bf7595f0e8b0c9a1cf8
nameofname <nameofemail@gmail.com>
2017-10-15 03:15:49 -0400

    almost...

--- js/almostSorted.js
+++ js/almostSorted.js
@@ -18,10 +18,7 @@ function printReverse(n1, n2) {
 
 function findSwapReverse (a) {
     const arr = new Array(a.length).fill('').map((x, i) => a[i]);
-    const len = arr.length;
     const segments = [];
-    const inflections = [];
-    // let direction = arr[0] < arr[1] ? "+" : "-";
 
     while (arr.length) {
         loop :
@@ -30,18 +27,19 @@ function findSwapReverse (a) {
             const prev = arr[idx - 1];
             const next = arr[idx + 1];
 
+            let negativeInflection = (prev <= curr) && (next < curr);
+            let positiveInflection = (prev >= curr) && (next > curr);
+
             if (prev === undefined) {
-                continue;
+                negativeInflection = false;
+                positiveInflection = false;
             }
 
-            const negativeInflection = (prev <= curr) && (next < curr);
-            const positiveInflection = (prev >= curr) && (next > curr);
-
-            console.log('test', positiveInflection, negativeInflection, prev, curr, next)
+            // console.log('test', positiveInflection, negativeInflection, prev, curr, next);
             if (positiveInflection || negativeInflection) {
                 const splitOn = negativeInflection ? idx : idx + 1;
                 segments.push(arr.splice(0, splitOn));
-                inflections.push(splitOn);
+                // inflections.push(splitOn);
                 break loop;
             } else if (next === undefined) {
                 segments.push(arr.splice(0, arr.length));
@@ -49,17 +47,21 @@ function findSwapReverse (a) {
         }
     }
 
+    console.log('segments', segments);
+
     if (segments.length > 3) {
         return console.log('no');
     }
 
     let negativeSegmentLength;
     let ordered = [];
+    const inflections = [];
 
     for (let j = 0; j < segments.length; j++) {
         const segment = segments[j];
         const isNegative = segment[1] < segment[0];
         let currSegment = segment;
+        inflections.push(ordered.length);
         if (isNegative) {
             negativeSegmentLength = segment.length;
             currSegment = segment.reverse();
@@ -82,13 +84,13 @@ function findSwapReverse (a) {
         return console.log('no');
     } else {
         const keyWord = (negativeSegmentLength === 2) ? 'swap' : 'reverse';
-        console.log(a);
+        // console.log(a);
         return console.log(`yes\n${keyWord} ${inflections.join(' ')}`);
     }
 }
 
 function processData(input) {
-    let [n, a] = _input.split("\n")
+    let [n, a] = _input.split("\n");
     a = a.split(' ');
     return findSwapReverse(a);
 }
@@ -105,10 +107,9 @@ process.stdin.on("end", function () {
 });
 
 
-// findSwapReverse([1, 2, 3, 6, 5, 4, 7 ,8]);
-// findSwapReverse([3, 2, 1, 4, 5, 6]);
-findSwapReverse([3, 1, 2]);
-findSwapReverse([1, 5, 4, 3, 2, 6]);
-
 // [1, 2, 3, 6, 5, 4, 7 ,8]
 // [3, 2, 1, 4, 5, 6]
+findSwapReverse([1, 2, 3, 6, 5, 4, 7 ,8]);
+findSwapReverse([3, 2, 1, 4, 5, 6]);
+findSwapReverse([3, 1, 2]);
+findSwapReverse([1, 5, 4, 3, 2, 6]);
