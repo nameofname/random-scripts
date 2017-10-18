30790a3d038312ab8767c22025374d6d6f57ea82
nameofname <nameofemail@gmail.com>
2017-10-18 00:59:31 -0400

    scaffolding for divisible sum pairs

---
+++ js/divisible-sum-pairs/divisible.js
@@ -0,0 +1,36 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function divisibleSumPairs(int, arr) {
+    console.log(int, arr)
+}
+
+function main() {
+    var n_temp = readLine().split(' ');
+    var n = parseInt(n_temp[0]);
+    var k = parseInt(n_temp[1]);
+    ar = readLine().split(' ');
+    ar = ar.map(Number);
+    var result = divisibleSumPairs(n, k, ar);
+    process.stdout.write("" + result + "\n");
+
+}
