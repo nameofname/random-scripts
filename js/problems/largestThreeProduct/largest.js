19bddc2cea2870ecc33b15a2cd0cbce242eab2fb
nameofname <nameofemail@gmail.com>
2017-10-19 01:08:30 -0400

    creating algorithm to find largest N numbers in an array

---
+++ js/problems/largestThreeProduct/largest.js
@@ -0,0 +1,43 @@
+// https://github.com/kennymkchan/interview-questions-in-javascript#array
+// 1.1 Given an array of integers, find the largest product yielded from three of the integers
+
+
+const addToTop = (arr, int) => {
+    let found;
+
+    if (!arr.length) {
+        arr.push(int);
+        return arr;
+    }
+
+    loop:
+    for (let i = arr.length; i > 0; i--) {
+        const curr = arr[i - 1];
+        console.log('comparison', arr, i, curr)
+        if (curr === undefined || curr < int) {
+            found = i;
+            break loop;
+        }
+    }
+    console.log('found', int, found)
+    if (found !== undefined) {
+        if (arr.length === 3) {
+            arr.shift();
+            arr.splice(found, 0, int);
+        } else {
+            arr.push(int)
+        }
+    }
+    return arr;
+};
+
+const lergestProductOfThree = arr => {
+    const largestThree = arr.reduce((a, curr) => {
+        return addToTop(a, curr);
+    }, []);
+    return largestThree;
+    // return largestThree[0] * largestThree[1] * largestThree[2];
+};
+
+
+console.log(lergestProductOfThree([1, 4, 5, 7, 3, 13, 10, 5, 11, 6, 9, 60, 2]));
