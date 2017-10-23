67297085b59b04a45f13dda2574b6f0c3d075451
nameofname <nameofemail@gmail.com>
2017-10-22 22:13:22 -0400

    intersection solution

---
+++ js/problems/interviewProblemsArrays/intersection.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const arr1 = [2, 2, 4, 1];
+const arr2 = [1, 2, 0, 2];
+
+const intersection = (a1, a2) => {
+    // choose the shorter array to make a hashmap of
+    const shorter = a1.length < a2.length ? a1 : a2;
+    const longer = shorter === a1 ? a2 : a1;
+    const map = shorter.reduce((o, int) => (Object.assign(o, { [int]: true })), {});
+
+    // using the map, and a new map for what was re-used, reduce to an array of integers in both arrays : 
+    const used = {};
+    return longer.reduce((arr, int) => {
+        if (!used[int] && map[int]) {
+            arr = [int, ...arr];
+        }
+        used[int] = true;
+        return arr;
+    }, []);
+};
+
+console.log(intersection(arr1, arr2)); // [2, 1]
