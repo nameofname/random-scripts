21beb0aec8e87751db2ebe062c2f36093aff156a
nameofname <nameofemail@gmail.com>
2017-10-23 22:42:13 -0400

    decimal to binary problem - done.

---
+++ js/problems/interviewProblemsRecursion/decimalToBinary.js
@@ -0,0 +1,53 @@
+"use strict";
+// 4.1 Write a recursive function that returns the binary string of a given decimal number Given 4 as the decimal input,
+// the function should return 100
+
+const _recur = (arr, int) => {
+    if (int === 0) {
+        return arr;
+    }
+
+
+    let carryBit = 1;
+    let i = arr.length - 1;
+
+    while (carryBit && i >= 0) {
+        arr[i] = arr[i] === 0 ? 1 : 0;
+        carryBit = (arr[i] === 0) ? 1 : 0;
+        --i;
+    }
+
+    if (i === - 1) {
+        arr.unshift(1);
+    }
+
+    return _recur(arr, --int);
+};
+
+const decimalToBinary = num => {
+    const arr = _recur([], num);
+    return arr.join('');
+};
+
+console.log(decimalToBinary(4)); // 100
+console.log(decimalToBinary(5)); // 101
+console.log(decimalToBinary(6)); // 110
+console.log(decimalToBinary(7)); // 111
+console.log(decimalToBinary(8)); // 1000
+
+
+/*
+ firstZeroIdx = firstZeroIdx ? firstZeroIdx : arr[i] === 0;
+ if (firstZeroIdx && arr[i] === 0) {
+ arr[i] = 1;
+ } else if (firstZeroIdx && arr[i] === 1) {
+ arr[i] = 0;
+ }
+ // arr[i] = arr[i] === 1 ? 0 : 1;
+ // if (arr[i] === 0) {
+ //     keepGoing = arr[i - 1] === 1;
+ // } else {
+ //     keepGoing = arr[i - 1] === 1;
+ // }
+
+ */
