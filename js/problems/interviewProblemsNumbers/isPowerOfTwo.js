16057c95239c9d327eb7b9c5d5193a2fbc3ba8bc
nameofname <nameofemail@gmail.com>
2017-10-23 23:40:57 -0400

    isPowerOfTwo function, done

---
+++ js/problems/interviewProblemsNumbers/isPowerOfTwo.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// 5.1 Given an integer, determine if it is a power of 2. If so, return that number, else return -1.
+// (0 is not a power of two)
+
+
+const isPowerOfTwo = num => {
+    if (num === 2 || num === 1) {
+        return true;
+    } else if (num === 0 || num % 2 !== 0) {
+        return false;
+    } else {
+        return isPowerOfTwo(num / 2);
+    }
+};
+
+console.log(isPowerOfTwo(4)); // true
+console.log(isPowerOfTwo(64)); // true
+console.log(isPowerOfTwo(1)); // true
+console.log(isPowerOfTwo(0)); // false
+console.log(isPowerOfTwo(-1)); // false
