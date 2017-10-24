7026a14f711e12dfd8aed63b65884cd77d6bb20c
nameofname <nameofemail@gmail.com>
2017-10-23 23:12:07 -0400

    binary search updated.

--- js/problems/interviewProblemsRecursion/binarySearch.js
+++ js/problems/interviewProblemsRecursion/binarySearch.js
@@ -2,7 +2,6 @@
 // 4.2 Write a recursive function that performs a binary search
 
 const _binary = (arr, term, left, right) => {
-    console.log('check', left, right)
     if (right - left < 2) {
         const l = arr[left] === term ? left : undefined;
         const r = arr[right] === term ? right : undefined;
@@ -25,4 +24,7 @@ const binarySearch = (arr, term) => {
 
 
 const arr1 = [1,4,5,7,9,10,12,16,17,19,26,27,29,30,47,48,50,52,57];
+const arr2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
 console.log(binarySearch(arr1, 16));
+console.log(binarySearch(arr2, 20));
+console.log(binarySearch(arr1, 28));
