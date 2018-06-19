c6769242ac07a8321833658196fd74fe5314bf6d
nameofname <nameofemail@gmail.com>
2018-06-19 18:53:29 -0400

    more detailed animation for boning

--- js/boningAnimation.js
+++ js/boningAnimation.js
@@ -10,12 +10,21 @@ const fuckingArr = [
     '        8=})',
 ];
 
+const completionArr = [
+    '8=====>~ ({})',
+    '8=====>~~({})',
+    '8=====> ~~{})',
+    '8=====>  ~~})',
+    '8=====>  (~})',
+    '8=====>  ({})',
+];
 let int = 0;
+let runs = 0;
 let direction = 1;
 let started = false;
 
 
-function doit() {
+function doit(callback) {
 
     if (fuckingArr[int]) {
         setTimeout(() => {
@@ -23,12 +32,34 @@ function doit() {
             console.log(fuckingArr[int]);
             if (int + 1 === fuckingArr.length || (int === 0 && started)) {
                 direction = direction * -1;
+                runs++;
             }
             int+= direction;  
             started = started || true;
-            doit();
+            if (runs > 7) {
+                int = 0;
+                callback();
+            } else {
+                doit(callback);
+            }
         }, 100);
     }
 }
 
-doit();
+function completion() {
+
+    setTimeout(() => {
+        process.stdout.write('\x1Bc')
+        console.log(completionArr[int]);
+        if (int + 1 === completionArr.length) {
+            int = 0;
+            runs++;
+        }
+        int++;
+        if (runs < 16) {
+            completion();
+        }
+    }, 100);
+}
+
+doit(completion);
