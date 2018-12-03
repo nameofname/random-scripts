220aea1ffa883a23d87dc8788ed1237b1b6ac282
nameofname <nameofemail@gmail.com>
2018-12-03 17:33:35 -0500

    added muscle man art

--- js/domDestroyer/src/index.js
+++ js/domDestroyer/src/index.js
@@ -1,9 +1,12 @@
 const { listenKeypress } = require('char-code-sequence');
 const { enable, disable } = require('./domDestroyer');
+const muscleAscii = require('./muscle-ascii');
+
 
 const startArr = "destroy!".split('').map(letter => letter.charCodeAt());
 const stopArr = "stop".split('').map(letter => letter.charCodeAt());
 listenKeypress(startArr, () => {
     enable();
-    console.log('DESTROYER ENABLED!');
+    console.log(muscleAscii);
 });
+listenKeypress(stopArr, stop);
