cd9e17d738a9c7a2a1d35cffc5f80a08427d4848
nameofname <nameofemail@gmail.com>
2018-12-03 17:57:56 -0500

    This thing actually works!

--- js/domDestroyer/src/index.js
+++ js/domDestroyer/src/index.js
@@ -3,7 +3,7 @@ const { enable, disable } = require('./domDestroyer');
 const muscleAscii = require('./muscle-ascii');
 
 
-const startArr = "destroy!".split('').map(letter => letter.charCodeAt());
+const startArr = "destroy".split('').map(letter => letter.charCodeAt());
 const stopArr = "stop".split('').map(letter => letter.charCodeAt());
 listenKeypress(startArr, () => {
     enable();
