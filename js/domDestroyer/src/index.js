e5e80aba6f095458ed4e7c05592009fe63c227ce
nameofname <nameofemail@gmail.com>
2018-12-04 11:54:41 -0500

    added function to remove classes that say 'scroll'

--- js/domDestroyer/src/index.js
+++ js/domDestroyer/src/index.js
@@ -7,6 +7,20 @@ const startArr = "destroy".split('').map(letter => letter.charCodeAt());
 const stopArr = "stop".split('').map(letter => letter.charCodeAt());
 listenKeypress(startArr, () => {
     enable();
+    removeScrollClasses();
     console.log(muscleAscii);
 });
 listenKeypress(stopArr, stop);
+
+function removeScrollClasses() {
+    // remove all classes from the DOM that contain the word 'Scroll'
+    // this is specifically so I can read recipes on my favorite site
+    var items = document.getElementsByTagName("*");
+    Array.from(items).forEach(ele => {
+        ele.classList.forEach(klass => {
+            if (klass.toLowerCase().includes('scroll')) {
+                ele.classList.remove(klass);
+            }
+        });
+    });
+}
