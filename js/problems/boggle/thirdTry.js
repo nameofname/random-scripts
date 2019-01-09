5f97aad13c3a0a16e92fe93277f241283ef8cdbc
nameofname <nameofemail@gmail.com>
2019-01-09 11:54:24 -0500

    move not working tries to file

--- js/problems/boggle/thirdTry.js
+++ js/problems/boggle/thirdTry.js
@@ -5,48 +5,6 @@ const board = [
     ['x', 'a', 'c', 'x'],
 ];
 
-/**
- * This guy does not work, but it's a good approach and should be fixed.
- */
-function _seek(points, wordArr, board, matches) {
-    const point = points.slice(-1)[0];
-    const [ x, y ] = point;
-    const letter = wordArr[0];
-    const currLetter = board[x] && board[x][y];
-
-    if (wordArr.length === 1 && letter === currLetter) {
-        // console.log('this case', matches)
-        matches.push(points);
-        return matches;
-    }
-
-    // if (letter === currLetter) {
-        const adjacentPoints = [ [ x - 1, y ], [ x + 1, y ], [ x, y - 1 ], [ x, y + 1 ] ];
-        // console.log('adjacentPoints, ', adjacentPoints)
-        return adjacentPoints.map(adjacentPoint => {
-            if (board[adjacentPoint[0]] && board[adjacentPoint[0]][adjacentPoint[1]]) {
-                const substr = wordArr.slice(1, wordArr.length);
-                const pointsNew = [...points, adjacentPoint];
-                return _seek(pointsNew, substr, board, matches);
-            }
-        });
-    // }
-}
-
-function boggleMatches_bak(word, board) {
-    const wordArr = word.split('');
-    const matches = [];
-    board.forEach((row, x) => {
-        row.forEach((letter, y) => {
-            const newMatches = _seek([[x, y]], wordArr, board, []);
-            if (newMatches) matches.concat(newMatches);
-        });
-    });
-    return matches;
-}
-// ----------------------------------------------------------------------
-
-
 function boggleMatches(word, board) {
 
     const matches = [];
@@ -86,16 +44,3 @@ function boggleMatches(word, board) {
 
 const matches = boggleMatches('cat', board);
 console.log('Found boggle matches for CAT : ', matches);
-// console.log(matches[0]);
-
-// TODO ! 
-// break the current implementation with a word that has 2 consecutive letters like 'caat'
-// because after it finds A it will look for A, and since it considers the spot it just
-// did in the last pass, it will get into an infinite loop ? no it won't. it will look for T
-// next, and not continue. Really the bug is that if you allow the algorithm to consider
-// the same position more than once for a given word then you can get a positive result for
-// a word like 'aaaaaaa' with just 2 'a's next to each other. 
-
-// TODO ! 
-// have the algorithm return a tree for performance, then create a reader that will give you
-// every array back from the tree. That would be fun. Fun.
