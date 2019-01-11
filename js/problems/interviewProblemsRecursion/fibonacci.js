c0fd32771670d082eef8ac6903f80618f5cd82c0
nameofname <nameofemail@gmail.com>
2019-01-11 14:23:14 -0500

    added fibonacci implementations

---
+++ js/problems/interviewProblemsRecursion/fibonacci.js
@@ -0,0 +1,31 @@
+function fibonacci(size){
+    const series = [1];
+
+    function _fibo(s) {
+        if (s !== 0) {
+            const last = series[series.length - 1] || 0;
+            const twoAgo = series[series.length - 2] || 0;
+            series.push(last + twoAgo);
+            --s;
+            return _fibo(s);
+        }
+         return series;
+    }
+
+    return _fibo(size - 1);
+}
+
+
+function fibonacci1(size, series = [1]) {
+    --size;
+    if (size) {
+        const last = series[series.length - 1] || 0;
+        const twoAgo = series[series.length - 2] || 0;
+        series.push(last + twoAgo);
+        return fibonacci1(size, series);
+    }
+    return series;
+}
+
+console.log(fibonacci(10));
+console.log(fibonacci1(10));
