c782dee3c97c33c4970d031732d3b90637ca32c1
nameofname <nameofemail@gmail.com>
2019-04-04 17:49:34 -0400

    this script is almost done yo

--- bash/getImages.sh
+++ bash/getImages.sh
@@ -1,17 +1,14 @@
 # /bin/bash
 
 URL=$1
-# FILENAME='tmp-total-garbage-string-its-fine-to-write-this-anywherei234yi2u3y42iu3y4ui324.txt'
-# ARRAY=()
 
 if [ -z $URL ] 
     then 
         echo 'please include a URL'
     else 
-        # HOST="$(node -e "const URL = require('url'); const url = URL.parse('$URL');  console.log(url.protocol + url.host);")";
 
-            # const fetch = require('fetch');
         # OUT="$(node -e "
+
         node -e "
             const url = require('url');
             const http = require('http');
@@ -28,54 +25,30 @@ if [ -z $URL ]
                 });
 
                 resp.on('end', () => {
+                    let matches = [];
 
-                    // second try - this is not done, its meant to be simpler, but i think a 3rd approach is brewing
                     extensions.forEach(extension => {
-                        const strArr = data
-                            .split('\<')
-                            .filter(s => s.includes(extension))
-                            .map(s => s.split(extension))
-                            .reduce((total, arr) => {
-
-                            }, [])
+                        const re = RegExp(/((https?)?:\/\/?[^\s]+.jpg)/g);
+                        const innerMatches = data
+                            .match(re)
+                            // dedupe based on host and path only
+                            .reduce((o, match) => {
+                                let u = url.parse(match);
+                                u = u.protocol + '//' + u.host + u.path;
+                                return Object.assign(o, { [u]: true })
+                            }, {})
+                        
+                        matches = matches.concat(Object.keys(innerMatches));
                     });
 
-                    // FIRST ATTEMPT - this one is too complex
-                    const strArr = data.split('\<');
-                    extensions.forEach(extension => {
-                        strArr.filter(s => s.includes(extension))
-                        .reduce((arr, s) => {
-                            s.split(extension)
-                                .forEach(ss => {
-                                    if (ss.includes('https') || ss.includes('http')) {
-                                        const scheme = ss.includes('https') ? 'https' : 'http';
-                                        arr.push(scheme + ss.split(scheme)[1]);
-                                    } else {
-                                        arr.push(ss + extension);
-                                    }
-                                    return arr;
-                                });
-                            return arr;
-                        }, [])
-                        .map(s => {
-                            if (s.includes('https') || s.includes('http')) {
-                                const scheme = s.includes('https') ? 'https' : 'http';
-                                return scheme + s.split(scheme)[1];
-                            }
-                            return `$HOST${s}`;
-                        })
-                        .forEach(s => console.log(s));
-                    });
-
+                    console.log(matches);
                 });
             })
-        ";
+        " | xargs wget --adjust-extension
+
         # ")";
-            
-                #    .map(s => url.parse(s))
-                #    .map(s => url.parse(s))
-                #    .forEach(s => console.log(s));
-            echo $OUT;
+        # echo $OUT;
+
         # " | xargs wget --adjust-extension
 fi
 
