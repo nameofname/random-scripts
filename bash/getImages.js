904fb2037c965498d973c1e6ecc105b080768eba
nameofname <nameofemail@gmail.com>
2019-04-05 17:47:37 -0400

    use isomorphic fetch - was having problems with the regular one

--- bash/getImages.js
+++ bash/getImages.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 
+require('isomorphic-fetch');
 const url = require('url');
-const http = require('http');
-const https = require('https');
-
 const URL = process.argv[2] || '';
 const u = url.parse(URL);
 if (!URL || !u.protocol) {
@@ -11,17 +9,11 @@ if (!URL || !u.protocol) {
     process.exit(1);
 }
 
-const host = u.protocol + u.host;
-const fetch = u.protocol.includes('https') ? https : http;
 const extensions = ['jpg', 'jpeg', 'png'];
 
-fetch.get(URL, resp => {
-    let data = '';
-    resp.on('data', (chunk) => {
-        data += chunk;
-    });
-
-    resp.on('end', () => {
+fetch(URL)
+    .then(d => d.text())
+    .then(data => {
         let matches = [];
 
         extensions.forEach(extension => {
@@ -40,4 +32,3 @@ fetch.get(URL, resp => {
 
         matches.forEach(s => console.log(s));
     });
-})
