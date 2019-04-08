1d1ef91e1574ab976cc5d68df01fef1ffd86202a
nameofname <nameofemail@gmail.com>
2019-04-08 12:04:51 -0400

    added another if statement example

--- bash/if-statements.sh
+++ bash/if-statements.sh
@@ -10,6 +10,13 @@ if [ -z $test ]
         echo "no."
 fi
 
+if [ -f ~/thing.txt ]
+    then
+        echo "I found ~/thing.txt";
+    else 
+        echo "There is no ~/thing.txt"
+fi
+
 
 # the one-line if statement will bomb out if you mis-place semicolons, 
 # or don't put spaces around the square brackets. notice the position of the semicolons 
