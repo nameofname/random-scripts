6f114ad118f41b5d346c457587757267117730a7
nameofname <nameofemail@gmail.com>
2019-07-06 13:41:17 -0400

    modified getimgs script

--- bash/getImages.js
+++ bash/getImages.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 require('isomorphic-fetch');
-const url = require('url');
+const { parse } = require('url');
 const URL = process.argv[2] || '';
-const u = url.parse(URL);
-if (!URL || !u.protocol) {
+const url = parse(URL);
+if (!URL || !url.protocol) {
     console.log(`Please include a valid URL, got this: ${URL}`);
     process.exit(1);
 }
@@ -17,14 +17,17 @@ fetch(URL)
         let matches = [];
 
         extensions.forEach(extension => {
-            const re = RegExp(`((https?)?:\/\/?[^\\s]+.${extension})`, 'g');
+            // const re = RegExp(`((https?)?:\/\/?[^\\s]+.${extension})`, 'g');
+            const re = RegExp(`(((https?)?:\/\/?)?[^\\s"']+.${extension})`, 'g');
             const innerMatches = data
                 .match(re) || [];
                 // dedupe based on host and path only
             const deduped = innerMatches.reduce((o, match) => {
-                    let u = url.parse(match);
-                    u = u.protocol + '//' + u.host + u.path;
-                    return Object.assign(o, { [u]: true })
+                    const url1 = parse(match);
+                    const useUrl = url1.host ? url1 : url;
+                    const path = url1.path.indexOf('/') === 0 ? url1.path : `/${url1.path}`;
+                    const location = `${useUrl.protocol}//${useUrl.host}${path}`;
+                    return Object.assign(o, { [location]: true })
                 }, {})
             
             matches = matches.concat(Object.keys(deduped));
