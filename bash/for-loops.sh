07346634070764fe8f68653c01dcd97e7e8e879e
nameofname <nameofemail@gmail.com>
2019-07-06 14:05:57 -0400

    added bash examples for loops

---
+++ bash/for-loops.sh
@@ -0,0 +1,13 @@
+for i in {0..3}
+do
+  echo "Number: $i"
+done
+
+
+for element in Hydrogen Helium Lithium Beryllium
+do
+  echo "Element: $element"
+done
+
+# can be done in 1 line : 
+for i in {1..3}; do echo "Number: $i"; done
