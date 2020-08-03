2ea41bdf351e4f4e6bb40a0c1724480ac43f93cf
nameofname <nameofemail@gmail.com>
2020-08-03 18:00:23 -0400

    comments, clean up

--- python/gpt_simple/get_training_data.py
+++ python/gpt_simple/get_training_data.py
@@ -15,11 +15,12 @@ output = None
 
 def build():
     global output
+    # Note* you have to use "is not" instead of != or else it gets converted into an elementwise operator.
     if (output is not None):
         return output
     # Read the file in with pandas read_csv function
     # I get an error on some rows, so I'm ignoring them with error_bad_lines=False
-    my_file = pd.read_csv(file_url, sep='\t', error_bad_lines=False)
+    raw_data = pd.read_csv(file_url, sep='\t', error_bad_lines=False)
 
     # For some reason, this crazy syntax returns all the values in the dataframe :
     # dataFrame[dataFrame[fieldName] === X]
@@ -29,10 +30,11 @@ def build():
     # Here's a post explaining it : https://heydenberk.com/blog/posts/demystifying-pandas-numpy-filtering/
 
     # Filter out rows where quality = 0
-    has_quality = my_file[my_file['Quality'] == 1]
+    has_quality = raw_data[raw_data['Quality'] == 1]
     # Filter out cases where there is no string 2, which happens sometimes, must be an issue with the decoding :
     has_data = has_quality[has_quality['#2 String'].notnull()]
 
+    # Add the training data sentence as a combination of String 1 and 2 from the file :
     # Idea for this line taken from : https://www.geeksforgeeks.org/adding-new-column-to-existing-dataframe-in-pandas/
     # Note - you need to use the axis = 1 argument (default 0) so apply is performed over rows no columns
     has_data['Training Sentence'] = has_data.apply(lambda row: "{} {} {}".format(row['#1 String'], separator, row['#2 String']), axis=1)
