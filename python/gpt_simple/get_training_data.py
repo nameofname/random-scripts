cf8d5351e7f8cdf90d6b84ec5486ea39f2fcaf6c
nameofname <nameofemail@gmail.com>
2020-08-03 16:49:50 -0400

    i now have real training data

--- python/gpt_simple/get_training_data.py
+++ python/gpt_simple/get_training_data.py
@@ -11,40 +11,30 @@ separator = '>>>>'
 file_name = 'msr_paraphrase_train.txt'
 file_url = 'file://{0}gpt_simple/data/{1}'.format(app_dir, file_name)
 
+output = 'grumbly weeners'
 
 def build():
+    if (output):
+        return output
     # Read the file in with pandas read_csv function
     # I get an error on some rows, so I'm ignoring them with error_bad_lines=False
     my_file = pd.read_csv(file_url, sep='\t', error_bad_lines=False)
 
-
-    # This example shows iterating through the dataframe using iterrows()
-    # for index, row in my_file.iterrows():
-    #     print(row['Quality'] > 0)
-
-    # For some reason, this crazy syntax returns all the values in the dataframe
-    # with a True or False if it matches the equivalency! 
+    # For some reason, this crazy syntax returns all the values in the dataframe :
+    # dataFrame[dataFrame[fieldName] === X]
+    # It works because the == turns into an "element wise" operator and returns a dataFrame of true / false values
+    # Pandas has a feature where that boolean dataFrame filters the list it's passed to
     # I can't tell if this is some sort of weird language feature is specific to pandas ?!
     # Here's a post explaining it : https://heydenberk.com/blog/posts/demystifying-pandas-numpy-filtering/
-    quality_list = my_file['Quality'] == 1
-    # print(quality_list)
-    # Then, in a crazy twist of events, you can filter the dataframe with the return value : 
-    has_quality = my_file[quality_list]
-    # print(has_quality)
-    # I also want to filter out cases where there is no string 2, which happens sometimes,
-    # apparently there's still something messed up with the decoding
-    has_data = my_file[my_file['#2 String'].notnull()]
 
-    # TODO ! Add the training data to each row : https://www.geeksforgeeks.org/adding-new-column-to-existing-dataframe-in-pandas/
-    has_data['Training Sentence'] = [row for row in has_data '{} {} {}'.format(row['#1 String'], separator, row['#2 String']]
-    return has_data
-    print(has_data.head())
-    # has_data.transform
+    # Filter out rows where quality = 0
+    has_quality = my_file[my_file['Quality'] == 1]
+    # Filter out cases where there is no string 2, which happens sometimes, must be an issue with the decoding :
+    has_data = has_quality[has_quality['#2 String'].notnull()]
 
-    # Now let's create a list of training inputs as explained in this paper on parapnrasing : 
-    # https://arxiv.org/pdf/1911.09661.pdf
-    # for index, row in has_data.iterrows():
-    #     # print('MISTER RONALD SAYS \n{} >>>> {}'.format(row['#1 String'], row['#2 String']))
-    #     print('{} {} {}'.format(row['#1 String'], separator, row['#2 String']))
+    # Idea for this line taken from : https://www.geeksforgeeks.org/adding-new-column-to-existing-dataframe-in-pandas/
+    # Note - you need to use the axis = 1 argument (default 0) so apply is performed over rows no columns
+    has_data['Training Sentence'] = has_data.apply(lambda row: "{} {} {}".format(row['#1 String'], separator, row['#2 String']), axis=1)
+    output = has_data
+    return output
 
-build()
