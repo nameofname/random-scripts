0cc8066b32e6897f17fed486128afadd98951972
nameofname <nameofemail@gmail.com>
2020-08-04 12:22:54 -0400

    i now write traning data to a file

--- python/gpt_simple/gpt_fine_tune.py
+++ python/gpt_simple/gpt_fine_tune.py
@@ -1,12 +1,18 @@
-import gpt_2_simple as gpt2
-from get_training_data import build
+# import gpt_2_simple as gpt2
+from get_training_data import build_data
 
-trainig_data = build()
+training_data = build_data()
+training_list = training_data['Training Sentence'].to_list()
+training_string = '\n'.join(training_list)
+# print('training data ready', training_string)
 
-print('training data ready')
+# Write the file to disc for use in Google Cloud : 
+f = open("training_data.txt", "w")
+f.write(training_string)
 
-gpt2.download_gpt2(model_name="124M")
 
-print('model downloaded')
 
+# ............
+# gpt2.download_gpt2(model_name="124M")
+# print('model downloaded')
 # Here's where I would fine tune GPT, but I don't have any tensors :|
