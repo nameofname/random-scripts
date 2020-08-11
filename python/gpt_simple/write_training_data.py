cb94b7906e16655bddb7903e042baef76ed4067f
nameofname <nameofemail@gmail.com>
2020-08-11 14:26:20 -0400

    updated the gpt training scripts, added output

--- python/gpt_simple/write_training_data.py
+++ python/gpt_simple/write_training_data.py
@@ -4,9 +4,13 @@ import datetime
 t = datetime.date.today()
 training_data = build_data()
 training_list = training_data['Training Sentence'].to_list()
+training_csv = training_data['Training Sentence'].to_csv(index = False, header = False)
 training_string = '\n'.join(training_list)
 # print('training data ready', training_string)
 
 # Write the file to disc for use in Google Cloud : 
-f = open("training_output/paraphrase_training_data_{}_{}_{}.txt".format(t.month, t.day, t.year), "w")
-f.write(training_string)
+# f = open("training_output/paraphrase_training_data_{}_{}_{}.txt".format(t.month, t.day, t.year), "w")
+# f.write(training_string)
+
+f = open("training_output/paraphrase_training_data_{}_{}_{}.csv".format(t.month, t.day, t.year), "w")
+f.write(training_csv)
