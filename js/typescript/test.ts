66bcc7a4edd48e56da3af48f12359c7c8759a94b
nameofname <nameofemail@gmail.com>
2021-02-25 18:09:47 -0500

    adding more TS tests

--- js/typescript/test.ts
+++ js/typescript/test.ts
@@ -15,10 +15,25 @@ function logStrArr(arg: string[]): string[] {
 
 logStrArr(['uno', 'dos', 'tres', 'quatro', 'cinqo', 'cinqo', 'seis']);
 
-// T is a generic 
+// T is a generic., it can be of any type, and the compiler 
+// will still know the return value's type 
 function identity<T>(arg: T): T {
+    console.log(`your identity ${arg}`);
     return arg;
 }
 
-console.log(identity('bla bla'));
+identity('bla bla');
+// TS knows this type! 
+const aNumber: number = identity(5);
 
+// Using the any type is kind of the same as using a generic,
+// but the compiler looses the info about the type after use
+function takesAnything(arg: any) :any {
+    console.log(`this could be anything ${arg}`);
+    return arg;
+}
+
+takesAnything('bob lob law');
+// TS doesn't know the type, it gets it wrong! 
+const idkWhat: number = takesAnything('55');
+console.log(typeof idkWhat);
