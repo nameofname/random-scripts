8c7d5415e4bf5ffa108295c3c4f3e1be09567b7d
nameofname <nameofemail@gmail.com>
2021-03-12 16:20:00 -0500

    working on integrating twitter with bigquery

--- python/twitter-trending/main.py
+++ python/twitter-trending/main.py
@@ -1,22 +1,31 @@
 import os
 import json
-import twitter_api as api
+import twitter_api as twitter
+import bigquery_api as bigquery
 
 def pp(o):
-    return print(json.dumps(o))
+    return print(json.dumps(o, indent=4, sort_keys=True))
 
+rules = twitter.get_rules()
+pp(rules)
 
-pp(api.delete_all_rules())
+pp(twitter.delete_all_rules())
 
-# api.set_rules([
-#     { "value": "luxury has:images", "tag": "luxury with img"},
-# ])
+rules = twitter.set_rules([
+    { "value": "luxury has:images", "tag": "luxury with img"},
+    # { "value": "cat has:images", "tag": "cat with img"},
+    # { "value": "dog has:images", "tag": "dog with img"}
+])
 
-pp(
-    api.set_rules([
-        { "value": "cat has:images", "tag": "cat with img"},
-        { "value": "dog has:images", "tag": "dog with img"}
-    ])
-)
+pp(rules)
 
-api.get_stream()
+def store_tweet(tweet):
+    # Legacy : 
+    # json_response = json.loads(response_line)
+    # callback(json.dumps(json_response, indent=4, sort_keys=True))
+    # print(tweet)
+    # pp(json.loads(tweet))
+    res = bigquery.store_record(tweet)
+    print(res) # TODO - can i use pp ? i love pp. 
+
+twitter.get_stream(store_tweet)
--- python/twitter-trending/twitter_api.py
+++ python/twitter-trending/twitter_api.py
@@ -1,10 +1,5 @@
 import requests
 import os
-import json
-import environment_variables
-
-# To set your enviornment variables in your terminal run the following line:
-# export 'BEARER_TOKEN'='<your_bearer_token>'
 
 
 bearer_token = os.environ.get("BEARER_TOKEN")
@@ -65,7 +60,7 @@ def delete_rules(rules):
     return response.json()
 
 
-def get_stream():
+def get_stream(callback):
     # url = "https://api.twitter.com/2/tweets/search/stream?tweet.fields=created_at,text,context_annotations,entities,in_reply_to_user_id,lang,non_public_metrics,organic_metrics,public_metrics"
     url = "https://api.twitter.com/2/tweets/search/stream?tweet.fields=created_at,text,context_annotations,entities,in_reply_to_user_id,lang,public_metrics"
     response = requests.get(url, headers=headers, stream=True,)
@@ -78,6 +73,4 @@ def get_stream():
         )
     for response_line in response.iter_lines():
         if response_line:
-            json_response = json.loads(response_line)
-            print(json.dumps(json_response, indent=4, sort_keys=True))
-            # return response.json()
+            callback(response_line)
