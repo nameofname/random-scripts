e1793eb746004db9ea5e0faaf48a67c247276c3b
nameofname <nameofemail@gmail.com>
2021-03-15 15:17:09 -0400

    use the 'with' keyword to avoid streaming error

--- python/twitter-trending/twitter_api.py
+++ python/twitter-trending/twitter_api.py
@@ -66,13 +66,13 @@ def get_stream(callback):
     url = "https://api.twitter.com/2/tweets/search/stream?tweet.fields=created_at,text,context_annotations,entities,in_reply_to_user_id,lang,public_metrics"
     # response = requests.get(url, headers=headers, stream=True,)
     with requests.get(url, headers=headers, stream=True,) as response:
-        # print(response.status_code)
-        if response.status_code != 200:
-            raise Exception(
-                "Cannot get stream (HTTP {}): {}".format(
-                    response.status_code, response.text
+            # print(response.status_code)
+            if response.status_code != 200:
+                raise Exception(
+                    "Cannot get stream (HTTP {}): {}".format(
+                        response.status_code, response.text
+                    )
                 )
-            )
-        for response_line in response.iter_lines():
-            if response_line:
-                callback(json.loads(response_line))
+            for response_line in response.iter_lines():
+                if response_line:
+                    callback(json.loads(response_line))
