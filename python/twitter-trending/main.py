b9d45506b2fd8c3f21a3969d6c886071b50b913f
nameofname <nameofemail@gmail.com>
2021-03-19 15:45:09 -0400

    added table schema json, small tweaks

--- python/twitter-trending/main.py
+++ python/twitter-trending/main.py
@@ -7,16 +7,13 @@ def pp(o):
     return print(json.dumps(o, indent=4, sort_keys=True))
 
 # # First we delete all the existing rules and set up a rule for luxury :
-# pp(twitter.delete_all_rules())
-# rules = twitter.set_rules([
-#     { "value": "luxury has:images", "tag": "luxury with img"},
-#     # { "value": "cat has:images", "tag": "cat with img"},
-#     # { "value": "dog has:images", "tag": "dog with img"}
-# ])
-# pp(rules)
+pp(twitter.delete_all_rules())
+rules = twitter.set_rules([
+    { "value": "luxury has:images", "tag": "luxury with img"},
+    # { "value": "cat has:images", "tag": "cat with img"},
+    # { "value": "dog has:images", "tag": "dog with img"}
+])
+pp(rules)
 
 # Then we stream tweets, and pass a callback to write to bigquery :
 twitter.get_stream(bigquery.store_record)
-# def test(rec):
-#     print(json.loads(rec))
-# twitter.get_stream(test)
