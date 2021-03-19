99a4649bbca4171bb534829aeab343ac68253d8d
nameofname <nameofemail@gmail.com>
2021-03-19 19:32:31 -0400

    working on batch job

--- python/twitter-trending/bigquery_api.py
+++ python/twitter-trending/bigquery_api.py
@@ -1,4 +1,4 @@
-from google.cloud import bigquery
+from google.cloud import bigquery, language_v1
 
 client = bigquery.Client()
 table_id = 'api-project-1065928543184.testing.twitter_luxury'
@@ -10,3 +10,22 @@ def store_record(rec):
     else:
         print('Encountered erorrs while inserting row {}\n{}'.format(errors, rec))
         print('Encountered erorrs while inserting row {}'.format(errors))
+
+def query(query):
+    query_job = client.query(query)  # Make an API request.
+    return query_job
+
+def analyze_entities(text_content):
+    client = language_v1.LanguageServiceClient()
+    request = {
+        'encoding_type': language_v1.EncodingType.UTF8,
+        'document': {
+            'content': text_content,
+            'language': 'en',
+            'type_': language_v1.Document.Type.PLAIN_TEXT # HTML also available
+        }
+    }
+    return client.analyze_entities(request)
+
+
+
