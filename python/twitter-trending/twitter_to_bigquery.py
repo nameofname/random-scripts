12b4c6780c2c98cd5fc582c99b51fadf3a04a3c6
nameofname <nameofemail@gmail.com>
2021-03-30 19:12:17 -0400

    starting to dockerize python

--- python/twitter-trending/twitter_to_bigquery.py
+++ python/twitter-trending/twitter_to_bigquery.py
@@ -6,18 +6,21 @@ import bigquery_api as bigquery
 def pp(o):
     return print(json.dumps(o, indent=4, sort_keys=True))
 
-# # First we delete all the existing rules and set up a rule for luxury :
-pp(twitter.delete_all_rules())
-rules = twitter.set_rules([
-    { "value": "luxury has:images", "tag": "luxury with img"},
-    # { "value": "cat has:images", "tag": "cat with img"},
-    # { "value": "dog has:images", "tag": "dog with img"}
-])
-pp(rules)
-
 def store_rec(rec):
-    print ('ima gonna store it', rec)
+    print ("Storing record in BigQuery\n", rec)
     return bigquery.store_record(rec, 'twitter_luxury')
 
-# Then we stream tweets, and pass a callback to write to bigquery :
-twitter.get_stream(store_rec)
+def main() :
+    # First we delete all the existing rules and set up a rule for luxury :
+    pp(twitter.delete_all_rules())
+    rules = twitter.set_rules([
+        { "value": "luxury has:images", "tag": "luxury with img"},
+        # { "value": "cat has:images", "tag": "cat with img"},
+        # { "value": "dog has:images", "tag": "dog with img"}
+    ])
+    pp(rules)
+    # Then we stream tweets, and pass a callback to write to bigquery :
+    twitter.get_stream(store_rec)
+
+if __name__ == "__main__":
+    main()
