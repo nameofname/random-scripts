9901235696147add8f3ac0c0003dbfeaee2d702a
nameofname <nameofemail@gmail.com>
2021-03-31 11:34:05 -0400

    tweaks to script

--- python/twitter-trending/main.py
+++ python/twitter-trending/main.py
@@ -2,5 +2,5 @@ import twitter_to_bigquery, batch_job
 from Repeater import Repeater
 
 twitter_to_bigquery.start()
-
-r = Repeater(batch_job.start, 1)
+r = Repeater(batch_job.start, 3600) # every hour
+r.start()
