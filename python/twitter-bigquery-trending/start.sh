cb70d33c86375c15edbbc500bdc869490f315494
nameofname <nameofemail@gmail.com>
2021-04-09 12:37:56 -0400

    comments

--- python/twitter-bigquery-trending/start.sh
+++ python/twitter-bigquery-trending/start.sh
@@ -1,5 +1,6 @@
 # /bin/bash
 
+# note: to debug when the container won't stat, add `sh` to the end.
 docker run \
     -it \
     -e GOOGLE_APPLICATION_CREDENTIALS_JSON="$GOOGLE_APPLICATION_CREDENTIALS_JSON" \
