7981c3f217c95881b638fb07364559c85961ecc9
nameofname <nameofemail@gmail.com>
2021-04-13 16:31:07 -0400

    working on time ranges for batch entity recognition

--- python/twitter-bigquery-trending/src/bigquery/batch_job.py
+++ python/twitter-bigquery-trending/src/bigquery/batch_job.py
@@ -1,26 +1,34 @@
 # import json
 import src.bigquery.bigquery_api as bqapi
-from datetime import datetime
+from datetime import datetime, timedelta
 
-def start():
-    # get a date range of the lst hour
-    now = datetime.utcnow()
-    date = now.strftime("%Y-%m-%d")
-    hour = now.strftime("%H")
-    last_hour = str(int(hour) - 1).zfill(2) # pad with zeros
-    min_sec = now.strftime("%M:%S")
-    start_date = "{} {}:{}".format(date, last_hour, min_sec)
-    end_date = "{} {}:{}".format(date, hour, min_sec)
+
+def get_time_range():
+    query = """select end_date
+        from `api-project-1065928543184.testing.twitter_luxury_entities`
+        order by end_date desc
+        limit 1;"""
+    print("executing date query :\n{}".format(query))
+    query = bqapi.query(query)
+    res = query.result()
+    start_time=False
+    for row in res:
+        start_time = row['end_date']
+
+    end_time = start_time + timedelta(hours=1)
+    return [start_time.strftime('%Y-%m-%d %H:%M:%S UTC'), end_time.strftime('%Y-%m-%d %H:%M:%S UTC')]
+
+def process_one_hour(time_range):
 
     # select for all tweets within that time range 
     query = """select *
         from `api-project-1065928543184.testing.twitter_luxury`
-        where data.created_at < '{}'
-        and data.created_at > '{}'
+        where data.created_at > '{}'
+        and data.created_at <= '{}'
         order by data.created_at desc
-        limit 100;""".format(end_date, start_date)
+        ;""".format(time_range[0], time_range[1])
     print("executing batch query :\n{}".format(query))
-    res = bqapi.query(query)
+    res = bqapi.query(query).result()
 
     # analyze entities in batch
     batch = ''
@@ -40,3 +48,13 @@ def start():
         # print(json.dumps(store))
         print('IM Not GOING TO STORE THISK>!>!>!>!', store)
         # bqapi.store_record(store, 'twitter_luxury_entities') 
+
+# TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+# TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+# TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+# this should work until you reach the current time. 
+def start():
+    time_range = get_time_range()
+    return process_one_hour(time_range)
+    # time_range = get_time_range()
+    # return print(time_range[0], time_range[1])
