e0ef1b5bcd1bdd41307006977812a6dac7c02911
nameofname <nameofemail@gmail.com>
2021-04-13 15:09:56 -0400

    2 different docker files working, build script added

---
+++ python/twitter-bigquery-trending/build.sh
@@ -0,0 +1,2 @@
+docker build -t twitter-bigquery-stream -f Dockerfile.1 .
+docker build -t twitter-bigquery-batch -f Dockerfile.2 .
--- python/twitter-bigquery-trending/start.sh
+++ python/twitter-bigquery-trending/start.sh
@@ -5,4 +5,4 @@ docker run \
     -it \
     -e GOOGLE_APPLICATION_CREDENTIALS_JSON="$GOOGLE_APPLICATION_CREDENTIALS_JSON" \
     -e BEARER_TOKEN="$BEARER_TOKEN" \
-    twitter-bigquery-trending
+    twitter-bigquery-stream
