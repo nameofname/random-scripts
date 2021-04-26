59fc29dc60543a742c3f1926a25e24fb5794808f
nameofname <nameofemail@gmail.com>
2021-04-26 16:26:09 -0400

    individual entity grapher is done

---
+++ python/twitter-bigquery-trending/graph_entity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import src.bigquery.graph_entity_mentions as graph_entity_mentions
+import sys
+import matplotlib.pyplot as plt
+
+if (not sys.argv[1]):
+    print('plesase provide an entity name to graph')
+
+res = graph_entity_mentions.query_entities(sys.argv[1])
+df = res.to_dataframe()
+df.plot.line(x = 'distinct_day', y = 'mentions')
+plt.show()
