f0d676d3000baeb5bc00e79c312f7a51996e2427
nameofname <nameofemail@gmail.com>
2021-06-21 23:56:23 -0500

    another solution to boggle

---
+++ js/problems/boggle/attempt6.js
@@ -0,0 +1,67 @@
+const board = [
+    ['x', 't', 'x', 'x'],
+    ['c', 'a', 't', 'x'],
+    ['x', 't', 'x', 'x'],
+    ['x', 'a', 'c', 'x'],
+];
+
+// 12:07 - 1:00
+function boggleMatches(board, str) {
+    const arr = str.split('');
+    const found = [];
+
+    function _getPositions(position) {
+        // const positions = [0, 1, 3, 4].map(int => {
+        //     return [position[0] - 2 + int, position[1] - 2 + int];
+        // });
+        // return positions;
+        return [
+            [position[0] - 1, position[1]],
+            [position[0], position[1] + 1],
+            [position[0], position[1] - 1],
+            [position[0] + 1, position[1]]
+        ]
+    }
+
+    // function _step(positions, nextLetter) {
+    //     const matches = [];
+    //     for (position of positions) {
+    //         if (board[position[0], position[1]] === nextLetter) {
+    //             matches.push(position);
+    //         }
+    //     }
+    //     return matches;
+    // }
+
+    function _recur(currPos, positionArr, letterArr) {
+        const currLetter = board[currPos[0]] ? board[currPos[0]][currPos[1]] : undefined;
+        console.log('checking', currPos, letterArr, currLetter)
+        if (currLetter === letterArr[0]) {
+            positionArr.push(currPos);
+            const trimmedArr = letterArr.slice(1, letterArr.length);
+            console.log('hit!!!', trimmedArr)
+            if (trimmedArr.length === 0) {
+                found.push(positionArr);
+                return;
+            } else {
+                const positionsToCheck = _getPositions(currPos);
+                console.log('what to chek', positionsToCheck)
+                for (nextPos of positionsToCheck) {
+                    _recur(nextPos, [...positionArr], trimmedArr)
+                }
+            }
+        }
+    }
+    
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board.length; j++) {
+            let currPos = [i, j];
+            _recur(currPos, [], arr);
+        }
+    }
+
+    return found;
+}
+
+
+console.log('Boggle matches for CAT : ', boggleMatches(board, 'cat'));
