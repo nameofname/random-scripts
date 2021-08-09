b906d87240f98fdc8585583fe5807f9034c65981
nameofname <nameofemail@gmail.com>
2021-08-09 15:16:22 -0500

    better stone game, still not working.

--- js/problems/leetcode/stoneGame.js
+++ js/problems/leetcode/stoneGame.js
@@ -1,19 +1,9 @@
 /**
-if one of the 2 outer has the advantage, assume you are going to pick that
-unless picking it would expose a bigger number
-
-else if the outer 2 are equal, 
-then you pick the one that exposes the smaller inner 
-
-there's a special case for if there are 3 piles left
-then you just choose the biggest outer, because either way you expose the same inner 
-
-if the 2 inner are also the same…
-then you can kind of pick at random… i think. you could try to go down a level to decide… 
-but let's punt on that and do random for now. 
-
-
-
+ * THIS DOES NOT WORK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * THE ANSWER IS ALWAYS RETURN TRUE. 
+ * If you carefully read the question, you can realize that the return value will always be true. 
+ * I got close with this implementation, but failed some test cases.
+ * 
  * @param {number[]} piles
  * @return {boolean}
  */
