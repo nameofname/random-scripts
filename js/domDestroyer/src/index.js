000f8539dafb10658cf12ec4e75cd3849eacbb14
nameofname <nameofemail@gmail.com>
2022-01-28 13:17:34 -0500

    dom destroyer will react to icon click

--- js/domDestroyer/src/index.js
+++ js/domDestroyer/src/index.js
@@ -5,17 +5,20 @@ const muscleAscii = require('./muscle-ascii');
 
 const startArr = "destroy".split('').map(letter => letter.charCodeAt());
 const stopArr = "stop".split('').map(letter => letter.charCodeAt());
-listenKeypress(startArr, () => {
+function turnOn() {
     enable();
     removeScrollClasses();
     console.log(muscleAscii);
-});
+}
+listenKeypress(startArr, turnOn);
 listenKeypress(stopArr, () => {
     disable();
     console.log('stop DOM Destroyer');
 });
-
-function removeScrollClasses() {
+// enable when the dom destroyer icon is clicked
+chrome.browserAction.onClicked.addListener(turnOn);
+ 
+ function removeScrollClasses() {
     // remove all classes from the DOM that contain the word 'Scroll'
     // this is specifically so I can read recipes on my favorite site
     var items = document.getElementsByTagName("*");
