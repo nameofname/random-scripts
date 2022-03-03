5aae617723c72ce75749dc5cd8430fbaefb880e7
nameofname <nameofemail@gmail.com>
2022-03-03 15:11:01 -0500

    updating with notes on if statements

--- bash/if-statements.sh
+++ bash/if-statements.sh
@@ -17,6 +17,11 @@ if [ -f ~/thing.txt ]
         echo "There is no ~/thing.txt"
 fi
 
+# IMPORTANT! There are 3 ways to write the test case for the IF condition : 
+# 1) "test" 2) single brackets 3) double brackets
+if test -f ~/.thing.txt; then echo 'one'; else echo 'nope'; fi
+if [ -f ~/.thing.txt ]; then echo 'two'; else echo 'nope'; fi
+if [[ -f ~/.thing.txt ]]; then echo 'three'; else echo 'nope'; fi
 
 # the one-line if statement will bomb out if you mis-place semicolons, 
 # or don't put spaces around the square brackets. notice the position of the semicolons 
