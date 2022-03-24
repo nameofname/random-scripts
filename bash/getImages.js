45627dc67ad4068b938eec9e2a35b20122ca4806
nameofname <nameofemail@gmail.com>
2022-03-24 12:03:02 -0400

    getimgs script updated to use puppeteer

--- bash/getImages.js
+++ bash/getImages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-require('isomorphic-fetch');
 const { parse } = require('url');
-const cheerio = require('cheerio');
 const { spawnSync } = require('child_process');
+const cheerio = require('cheerio');
+const puppeteer = require("puppeteer");
 
 const URL = process.argv[2] || '';
 const url = parse(URL);
@@ -12,8 +12,6 @@ if (!URL || !url.protocol) {
     console.log(`Please include a valid URL, got this: ${URL}`);
     process.exit(1);
 }
-
-const extensions = ['jpg', 'jpeg', 'png'];
 
 // adds a numeric suffix to the file name, before the extension
 function enumerateFilename(fileName) {
@@ -32,59 +30,50 @@ function enumerateFilename(fileName) {
     }
 }
 
-fetch(URL)
-    .then(d => d.text())
-    .then(data => {
-        let matches = [];
+function suffixImgs(imgArr) {
+    return imgArr.reduce((map, filePath) => {
+        let fileName = filePath.split('/').slice(-1)[0];
+        while (map[fileName]) {
+            fileName = enumerateFilename(fileName);
+        }
+        return Object.assign(map, {[fileName]: filePath});
+    }, {});
+}
 
-        // get all of the images using using a Regex
-        extensions.forEach(extension => {
-            const re = RegExp(`(((https?)?:\/\/?)?[^\\s"']+.${extension})`, 'g');
-            const innerMatches = data
-                .match(re) || [];
-                // dedupe based on host and path only
-            const deduped = innerMatches.reduce((o, match) => {
-                    const url1 = parse(match);
-                    const useUrl = url1.host ? url1 : url;
-                    const path = (url1.path || '').indexOf('/') === 0 ? url1.path : `/${url1.path}`;
-                    const location = `${useUrl.protocol}//${useUrl.host}${path}`;
-                    return Object.assign(o, { [location]: true })
-                }, {})
-            
-            matches = matches.concat(Object.keys(deduped));
-        });
+async function getData(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const data = await page.content();
+    await browser.close();
+    return data;
+}
 
-        // TODO !!!!!!! 
-        // There are 2 cases to solve for ... 
-        // 1. sometimes images don't have extensions
-        // 2. sometimes images are rendered very soon after load
-        // The following page is an example of both : https://www.zipcomic.com/comedy-comics-1948-issue-1 
-        // So I should check for images that don't match the regex by parsing image tags src attribute, AND 
-        // find a way to get client rendered images
-        const $ = cheerio.load(data);
-        const imgs = $('img').attr('class');
-        console.log('imgs', imgs)
-
-        console.log('matches', matches)
-        // if there are duplicate file names, number them to avoid conflict : 
-        const fileMap = matches.reduce((map, filePath) => {
-            let fileName = filePath.split('/').slice(-1)[0];
-            while (map[fileName]) {
-                fileName = enumerateFilename(fileName);
-            }
-            map[fileName] = filePath;
-            return map;
-        }, {});
-
-        spawnSync('mkdir', ['-p', 'image-downloads']);
-        // spawnSync('cd', ['image-downloads']);
-        process.chdir('./image-downloads');
-        Object.keys(fileMap).forEach(fileName => {
-            console.log(`fetching ${fileName} from ${fileMap[fileName]}`);
-            // spawnSync('wget', ['-O', fileName, fileMap[fileName], '-P', './image-downloads']);
-            spawnSync('wget', ['-O', fileName, fileMap[fileName]]);
-        });
+async function parseImgs(data) {
+    const $ = cheerio.load(data);
+    const imgs = [];
+    $("img").each(function () {
+        imgs.push(this.attribs.src);
     });
+    return imgs;
+}
 
-// console.log(enumerateFilename('bla--1.jpg'))
+function saveImgs(fileMap) {
+    spawnSync('mkdir', ['-p', 'image-downloads']);
+    process.chdir('./image-downloads');
+    Object.keys(fileMap).forEach(fileName => {
+        console.log(`fetching ${fileName} from ${fileMap[fileName]}`);
+        // spawnSync('wget', ['-O', fileName, fileMap[fileName], '-P', './image-downloads']);
+        spawnSync('wget', ['-O', fileName, fileMap[fileName]]);
+    });
+}
 
+async function getImgs(url) {
+    const data = await getData(url);
+    const imgs = await parseImgs(data);
+    const fileMap = suffixImgs(imgs);
+    // console.log(fileMap)
+    return saveImgs(fileMap);
+}
+
+getImgs(URL)
