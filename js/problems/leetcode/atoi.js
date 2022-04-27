58ea2b6743c348ca74381261bfa09ecb5856bbef
nameofname <nameofemail@gmail.com>
2022-04-26 23:42:21 -0400

    more problems

--- js/problems/leetcode/atoi.js
+++ js/problems/leetcode/atoi.js
@@ -4,18 +4,36 @@
  */
 var myAtoi = function(s) {
     const range = [ -Math.pow(2, 31), Math.pow(2, 31) - 1 ];
-    const arr = String(s).split('');
-    let sign;
+    const arr = String(s).trim().split('');
+    let sign = 1;
     if (arr[0] === '-' || arr[0] === '+') {
         sign = arr.shift() === '-' ? -1 : 1;
     }
-    let n;
-    while (arr[0] === '0') {
-        arr.shift();
+    const digits = [];
+    const re = /\d/;
+    loop:
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '0' && !digits.length) {
+            continue;
+        } else if (re.test(arr[i])) {
+            digits.push(arr[i]);
+        } else {
+            break loop;
+        }
     }
-    return Number(arr.join('')) * sign;
+
+    const ret = Number(digits.join('')) * sign;
+    if (ret < range[0] || ret > range[1]) {
+        return ret < range[0] ? range[0] : range[1];
+    }
+    return ret;
 };
 
 console.log(myAtoi('123'));
 console.log(myAtoi('000234'));
 console.log(myAtoi('-000234'));
+console.log(myAtoi('2147483648'));
+console.log(myAtoi('2147483649'));
+console.log(myAtoi("4193 with words"));
+console.log(myAtoi("   123"));
+console.log(myAtoi("-91283472332"));
