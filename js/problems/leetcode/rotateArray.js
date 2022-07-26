367cfb25f80a8175a52f24fe7418c9bbf1e139f5
nameofname <nameofemail@gmail.com>
2022-07-25 21:58:29 -0400

    doctoring up 2 more problems

--- js/problems/leetcode/rotateArray.js
+++ js/problems/leetcode/rotateArray.js
@@ -3,16 +3,23 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
- function rotate(nums, k) {
-    const a = new Array(nums.length);
-    k = k % nums.length;
-    for (let i = 0; i <= k; i++) {
-        a[k - i] = nums[nums.length - i]
+function rotate(nums, k) {
+    const tmp = nums.slice(-1 * k);
+    for (let i = nums.length - 1; i >= k; i--) {
+        nums[i] = nums[i - k];
     }
-    for (let i = k; i < nums.length; i++) {
-        a[i] = nums[i - k];
+    for (let j = 0; j < tmp.length; j++) {
+        nums[j] = tmp[j];
     }
-    return a;
+    return nums;
 };
+
+// niave solution exceeds the time limit.
+function rotate_niave(nums, k) {
+    for (let i = 0; i < k; i++) {
+        nums.unshift(nums.pop());
+    }
+    return nums;
+}
 
 console.log(rotate([1,2,3,4,5,6,7], 3));
