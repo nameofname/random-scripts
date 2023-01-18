65f64fb6129b98e520f256e5dc8a5b179b886bb1
Ron Williams <nameofemail@gmail.com>
2023-01-18 11:52:57 -0500

    updating java examples

--- java/hello/Ternary.java
+++ java/hello/Ternary.java
@@ -1,9 +1,7 @@
 class Ternary {
     private static boolean boo = false;
     public static void main(String[] args) {
-        // String value = boo ? 'yes' : 'no';
-        // System.out.println("result: " + value);
-        boolean value = ('foo' eq "foo");
+        String value = boo ? "yes" : "no" + " more strings";
         System.out.println("result: " + value);
     }
 }
