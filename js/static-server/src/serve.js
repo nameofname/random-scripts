ece64d12517331847b924708a765d8ca4af23869
nameofname <nameofemail@gmail.com>
2023-12-06 10:22:35 -0500

    static server now accepts POST requests, for fun!

--- js/static-server/src/serve.js
+++ js/static-server/src/serve.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import yargs from 'yargs';
 import express from 'express';
 import getIndex from './getIndex.js';
+import bodyParser from 'body-parser';
 
 
 const args = yargs(process.argv.slice(2))
@@ -39,8 +40,17 @@ function delayer(req, res, next) {
 }
 
 function logger(req, res, next) {
-    console.log(req.method, req.url);
+    console.log(req.method, req.url, req.body);
     next();
+}
+
+// Respond to POST requests, mostly for testing purposes.
+function postResponder (req, res, next) {
+    if (Object.keys(req.body).length) {
+        req.body.staticServer = true;
+        return res.json(req.body);
+    }
+    return next();
 }
 
 export default function serve() {
@@ -49,7 +59,14 @@ export default function serve() {
         app.get('/', delayer, logger, function(req, res) {
             res.send(getIndex(_path));
         });
-        app.use(delayer, logger, express.static(_path, { maxAge: 5000 }));
+        app.use(
+            delayer,
+            bodyParser.json(),
+            logger,
+            postResponder,
+            express.static(_path, { maxAge: 5000 })
+        );
+
     } else {
         app.use(delayer, logger, (req, res) => {
             res.sendFile(_path, { maxAge: 5000 });
